9223c0f8cab7ce4d0b963f1ef780dda0e3fcb89e
Ilias Fasikhov <ilias.fasikhov@burberry.com>
2020-06-15 16:13:13 +0300

    EPMCJSCC-727 PowerBi filters via select

--- src/client/containers/remote-control.tsx
+++ src/client/containers/remote-control.tsx
@@ -2,25 +2,49 @@ import * as React from "react";
 import { useCallback } from "react";
 import { Draggable, Droppable } from 'react-drag-and-drop'
 
-const filter = {
-    $schema: "http://powerbi.com/product/schema#basic",
-    target: {
-        table: "Store",
-        column: "Chain"
-    },
-    operator: "In",
-    values: ["Lindseys"]
-};
+const filters = [{
+    id: 0,
+    name: '---'
+},{
+    id: 1,
+    name: 'Lindseys',
+    data: {
+        $schema: "http://powerbi.com/product/schema#basic",
+        target: {
+            table: "Store",
+            column: "Chain"
+        },
+        operator: "In",
+        values: ["Lindseys"]
+    }
+}, {
+    id: 2,
+    name: 'Fashions Direct',
+    data: {
+        $schema: "http://powerbi.com/product/schema#basic",
+        target: {
+            table: "Store",
+            column: "Chain"
+        },
+        operator: "In",
+        values: ["Fashions Direct"]
+    }
+}];
 
 const RemoteController = ({ dashboards, screens, setScreenBoard, setFilter }): JSX.Element => {
     const onDropElement = useCallback(screen => ({ dashboard }) =>{
-        console.log('moved', dashboard, ' to ', screen);
         setScreenBoard(screen, dashboard);
     }, []);
 
     const onApplyFilter = useCallback((dashboard) => e => {
-        console.log('dashboard', dashboard);
-        setFilter(dashboard, filter);
+        const { target } = e;
+        const filterId: number = target.value * 1;
+        const filter = filters.find(({ id }) => id === filterId);
+        if (filter?.data) {
+            setFilter(dashboard, filter.data);
+        } else {
+            // TODO: resetFilterFunctionality - can be done by calling report.resetFilters();
+        }
     }, []);
 
     return <div className="controlContainer">
@@ -32,7 +56,12 @@ const RemoteController = ({ dashboards, screens, setScreenBoard, setFilter }): J
                         <React.Fragment key={element}>
                         <Draggable type="dashboard" data={element}>
                             <li>
-                                {element} <button name="filter" onClick={onApplyFilter(element)}>filter </button>
+                                <span className="dashboardsList__item-name">{element}</span>
+                                <label className="dashboardsList__item-filters"><span>Filters:</span>
+                                    <select name="filters" onChange={onApplyFilter(element)} defaultValue="0">
+                                        {filters.map(filter => <option key={filter.id} value={filter.id}>{filter.name}</option>)}
+                                    </select>
+                                </label>
                             </li>
                         </Draggable>
                         </React.Fragment>
@@ -48,7 +77,6 @@ const RemoteController = ({ dashboards, screens, setScreenBoard, setFilter }): J
                         <Droppable key={key}
                             types={['dashboard']} // <= allowed drop types
                             onDrop={onDropElement(element)} >
-                            {/*onDrop={onDropElement(element)}>*/}
                             <li>Display #{element.id} {element.dashboard ? ` - ${element.dashboard}` : ""}</li>
                         </Droppable>
                     )
